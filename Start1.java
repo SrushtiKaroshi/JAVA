7dadb5d481e7c985bb5589b45e5b807431e98d08
karoshisrushti@gmail.com <srushtikaroshi10@gmail.com>
2023-02-09 08:29:30 +0530

    initial commit

---
+++ Start1.java
@@ -0,0 +1,21 @@
+import java.lang.*;
+
+
+class Start1 {
+    public static void main(String args[]) {
+        
+        System.out.println("Namaskar");
+
+        byte by = 10;
+        short sh = 20;
+        char ch = 'a';
+        int i = 21;
+        float f = 3.14f;
+        double d = 3.1483;
+        long l = 1234;
+        boolean b = true;
+
+        System.out.println("Inside main");
+
+    }
+}
